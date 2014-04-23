dfd4d30834f5d3ba5f95d2d2d13a710e2a8c1537
dead_horse <dead_horse@qq.com>
2014-04-23 14:33:07 +0800

    refactor

--- index.js
+++ index.js
@@ -15,14 +15,26 @@
 
 var thunkify = require('thunkify');
 
-var props = ['gists', 'gitdata', 'issues', 'authorization', 'orgs',
- 'statuses', 'pullRequests', 'repos', 'user' , 'events', 'search', 'markdown'];
+var props = exports.props = [
+  'gists',
+  'gitdata',
+  'issues',
+  'authorization',
+  'orgs',
+  'statuses',
+  'pullRequests',
+  'repos',
+  'user' ,
+  'events',
+  'search',
+  'markdown',
+];
 
 
 module.exports = function (github) {
   var coApis = {};
   props.forEach(function (prop) {
-    var apis = github[prop] || [];
+    var apis = github[prop] || {};
     coApis[prop] = {};
     for (var key in apis) {
       coApis[prop][key] = thunkify(apis[key]);
